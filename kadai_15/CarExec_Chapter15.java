71274eac97b89478371ee4419ad2f1f77cb40d06
yui3725 <flute.yui@gmail.com>
2024-11-14 19:30:27 +0900

    Add files via upload

---
+++ kadai_15/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package kadai_15;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		Car_Chapter15 Car = new Car_Chapter15(1, 10);
+		Car.gearChange(3);
+		Car.run();
+	}
+
+}
---
+++ kadai_15/Car_Chapter15.java
@@ -0,0 +1,30 @@
+
+package kadai_15;
+
+public class Car_Chapter15 {
+	// フィールド
+	private int gear = 1;
+	private int speed = 10;
+	
+	// コンストラクタ
+	public Car_Chapter15( int gear, int speed ) {
+		this.gear  = gear;
+		this.speed = speed;
+	}
+	// メソッド
+	public void gearChange(int afterGear) {
+		switch(afterGear) {
+		case 1  -> speed = 10;
+		case 2  -> speed = 20;
+		case 3  -> speed = 30;
+		case 4  -> speed = 40;
+		case 5  -> speed = 50;
+		default -> speed = 10;
+		}
+		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+	}
+	public void run() {
+		System.out.println("速度は時速" + speed + "kmです" );
+	}
+
+}
